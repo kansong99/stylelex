a148039c826280841f67c3efc1e8c54be6002cda
Kofi <w.fifi99@gmail.com>
2020-05-12 17:06:29 -0400

    debugged numb.py

--- numb.py
+++ numb.py
@@ -1,8 +1,8 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_md")
 
-text = ("Kofi has bought 123,564 grapes")
+text = ("Kofi has $1000")
 
 
 bigNums = ['hundred', 'thousand', 'million', 'billion', 'trillion']
@@ -30,14 +30,13 @@ def hasSign(text, start_char): #function to tell if dollar or $ is used
 
 
 doc = nlp(text)
-
 for ent in doc.ents:
 	first = text[ent.start_char]
 	last = text[ent.end_char - 1]
 	word = ent.text
-	if ent.label_ is 'CARDINAL' or ent.label_ is 'MONEY' or ent.label_ is 'QUANTITY' or ent.label_ is 'DATE':
+	if ent.label_ is 'CARDINAL' or ent.label_ is 'MONEY' or ent.label_ is 'QUANTITY' or ent.label_ is 'DATE' or ent.label_ is 'PERCENT':
 		startCheck = ent.start_char #check if we are starting a sentence.
-		ending = text[ent.end_char - 1 :ent.end_char + 1] #check to validate last two chars are not 00
+		ending = text[-2:] #check to validate last two chars are not 00
 		if ((startCheck > 3) and (text[startCheck - 2: startCheck] is '. ' or text[startCheck - 3: startCheck] is '.  ')) or startCheck is 0: #starts sentence
 			if first.isnumeric() and ent.label_ is not 'DATE':
 				print("spell out numbers when they start sentence")
@@ -49,18 +48,21 @@ for ent in doc.ents:
 				if ent.text[len(ent.text) - 8] == ' ':
 					last = ent.text[len(ent.text) - 9]
 					word = ent.text[:len(ent.text) - 8]
+					ending = word[len(word) - 2:]
 				else:
 					last = ent.text[len(ent.text) - (8)]
 					word = ent.text[:len(ent.text) - 7]
+					ending = word[len(word) - 2:]
+			print(ending)
 			if first.isnumeric() and last.isnumeric(): #this means word is a number, should be greater than 100 or decimal
-				if (int(word) >= 100 and ending is not '00') or '.' in ent.text: #This is good and means we should use $
-					if hasSign(text, start) is False:
+				if (int(word) >= 100 and ending != '00') or '.' in word: #This is good and means we should use $
+					if hasSign(text, startCheck) is False:
 						print("You should use $ when money is numeral")
-				elif ending is '00':
+				elif ending == '00':
 					if hasSign(text, startCheck): #if ending indicates text should be written and they used $
 						print("Write out round numbers and use dollars instead of $")
 					else: #they did not use $ but did not spell out round number
-						print("Write out round numbers e.g, twenty-seven thousand")
+						print("Write out round numbers e.g, twenty-seven thousand dollars")
 				elif int(word) < 100:
 					if hasSign(text, startCheck):
 						print("spell out numbers less than 100 and use dollars instead of $")
@@ -78,6 +80,10 @@ for ent in doc.ents:
 						print("don't spell out number greater than 100 and use $ instead of dollars")
 				if hasSign(text, startCheck):
 					print("dollars should be used instead of $ for numbers less than or equal to 100")
+
+
+		elif ent.label_ is 'QUANTITY':
+			print("fuckk")
 
 
 
