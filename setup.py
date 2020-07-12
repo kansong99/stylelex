de9f876ea5fbb618d8b974545b969ad07922ba89
Bliss Perry <jordanperry@college.harvard.edu>
2020-07-12 11:47:10 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='website_test',
+    packages=['website_test'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
