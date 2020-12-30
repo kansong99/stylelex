348c42d2cd371518d83ef5f5c3683767967770cf
Kofi <w.fifi99@gmail.com>
2020-12-30 04:29:47 -0500

    create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// requirements
+
+var gulp = require('gulp');
+var browserify = require("browserify");
+var babel = require('gulp-babel');
+var del = require('del');
+var size = require('gulp-size');
+var watch = require('gulp-watch');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+
+// tasks
+gulp.task('transform', function(done) {
+    return gulp.src('website_test/static/scripts/jsx/*.jsx')
+        .pipe(babel({
+            presets: ["@babel/react", "@babel/env"]
+        }))
+        .pipe(gulp.dest('website_test/static/scripts/dist'));
+        done();
+});
+
+gulp.task('js', gulp.series('transform', function(done) {
+    // Assumes a file has been transformed from
+    // ./app/src/main.jsx to ./app/dist/main.js
+    console.log("mee")
+    return browserify('website_test/static/scripts/dist/main.js')
+    	.bundle()
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('website_test/static/scripts/js/'));
+     done();
+}));
+
+gulp.task('default', gulp.series('js', function(done) {
+    //watch('website_test/static/scripts/jsx/*.jsx');
+    done();
+}));
+
+//gulp.task('default', gulp.series(function(done) {    
+    // task code here
+//    done();
+//}));
+
+
+// gulp.task('transform', function () {
+//   var stream = gulp.src('website_test/static/scripts/jsx/*.js')
+//     .pipe(gulpBrowser.browserify({transform: ['reactify']}))
+//     .pipe(gulp.dest('website_test/static/scripts/js/'))
+//     .pipe(size());
+//   return stream;
+// });
+
+// gulp.task('del', function () {
+//   // add task
+// });
+
+// gulp.task('default', gulp.series("transform", function(done) {
+//   done();
+// }));
---
+++ website_test/static/scripts/hello.js
@@ -0,0 +1,11 @@
+	
+      var realPython = React.createClass({
+        render: function() {
+          return (<h2>Greetings, from Real Python!</h2>);
+        }
+      });
+
+      ReactDOM.render(
+        React.createElement(realPython, null),
+        document.getElementById('content')
+      );
