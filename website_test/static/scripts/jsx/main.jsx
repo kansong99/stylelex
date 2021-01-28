4dae8779f8056bb8ec628a9b14554b3881a23bc9
Aaron Dang <aarondang1111@gmail.com>
2021-01-27 23:39:19 -0500

    revert back to classes

--- website_test/static/scripts/jsx/main.jsx
+++ website_test/static/scripts/jsx/main.jsx
@@ -7,6 +7,7 @@ function Sect(props) {
   )
 } 
 
+/*
 function TextBox() {
   const [shown, setShown] = useState(false);
   const final = [];
@@ -17,7 +18,66 @@ function TextBox() {
   return(<div onMouseEnter={() => setShown(true)}
            onMouseLeave={() => setShown(false)} contentEditable = 'true' >{shown && final}</div>)
 }
+*/
 
+class TextBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShown: false};
+
+    // This binding is necessary to make `this` work in the callback
+    this.hoverOff = this.hoverOff.bind(this);
+    this.hoverOn = this.hoverOn.bind(this);
+  }
+
+  hoverOn() {
+    this.setState(state => ({
+      isShown: true
+    }));
+  }
+
+  hoverOff() {
+    this.setState(state => ({
+      isShown: false
+    }));
+  }
+
+  
+  render() {
+    /*
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+    */
+  
+  const final = []
+  for (const element of soln) {
+    final.push(<Sect contentEditable='true' chunk={element}></Sect>)
+  }
+
+  return(<div onMouseEnter={() => hoverOn()}
+    onMouseLeave={() => hoverOff()} contentEditable = 'true' >{this.state.isShown && final}</div>)
+  }
+}
+  
+
+
+/*
+  render () {
+    const final = [];
+    
+    for ( const element of this.state.value) {
+      final.push(<Sect contentEditable='true' chunk={element}></Sect>)
+    }
+    console.log(final)
+    return <div contentEditable='true'
+    onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>{final}</div>
+  }
+}
+*/
 
 ReactDOM.render(<TextBox></TextBox>, document.getElementById('main'));
 // class TextBox extends React.Component {
